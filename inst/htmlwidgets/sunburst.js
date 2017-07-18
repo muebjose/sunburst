6d090ae4603f4afc6812bf5cd1e95869f670684b
muebjose <eby.jose@mu-sigma.com>
2017-07-18 15:44:57 +0530

    Update sunburst.js
    uncommented original code and commented edited code

--- inst/htmlwidgets/sunburst.js
+++ inst/htmlwidgets/sunburst.js
@@ -188,14 +188,14 @@ var draw = function (el, instance, dispatch_) {
   // Fade all but the current sequence, and show it in the breadcrumb trail.
   function mouseover(d) {
 
-    //var percentage = (100 * d.value / totalSize).toPrecision(3);
-    //var percentageString = percentage + "%";
-    //if (percentage < 0.1) {
-    //  percentageString = "< 0.1%";
-    //}
+    var percentage = (100 * d.value / totalSize).toPrecision(3);
+    var percentageString = percentage + "%";
+    if (percentage < 0.1) {
+      percentageString = "< 0.1%";
+    }
 
-    var percentage = (d.value/d.length).toPrecision(3);
-    var percentageString = "Score: " + percentage;
+    //var percentage = (d.value/d.length).toPrecision(3);
+    //var percentageString = "Score: " + percentage;
 	  
     var countString = [
         '<span style = "font-size:.7em">',
